2b0ccc8cdc2738088ebac73930a23c49cc32ee09
arunparkcse <arunparkcse@gmail.com>
2026-05-14 16:01:59 +0530

    feat: implement base application pages and navigation components

--- labelpack-ng/src/app/app.ts
+++ labelpack-ng/src/app/app.ts
@@ -10,15 +10,10 @@ import { AnimationService } from './services/animation.service';
 })
 export class App implements AfterViewInit {
   constructor(private anim: AnimationService) {}
-
   ngAfterViewInit() {
-    // Small delay ensures child components (navbar/footer) are in DOM
     setTimeout(() => {
       this.anim.initPreloader();
       this.anim.initCursor();
-      this.anim.initNav();
-      this.anim.initMobileMenu();
-      this.anim.initTouchDropdown();
     }, 0);
   }
 }
